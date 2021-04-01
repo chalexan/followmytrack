fb4a00c8bc708b1a6757b8fda138c3a53544fd2c
chalexan <cherednychenko.alexandr@gmail.com>
2021-04-01 22:50:08 +0300

    testing heroku

--- connectDb.js
+++ connectDb.js
@@ -7,8 +7,8 @@ const options = {
   useFindAndModify: false,
   };
 
-//  const dbPath = 'mongodb://localhost:27017/followmytrack';
-const dbPath = process.env.DB_PATH;
+  const dbPath = 'mongodb+srv://c:Protekt2019@cluster0.qju1t.mongodb.net/followmytrack?retryWrites=true&w=majority';
+//const dbPath = process.env.DB_PATH;
 const connect = () => {
   mongoose.connect(dbPath, options)
   .then(() => console.log('Connected to DB'))
